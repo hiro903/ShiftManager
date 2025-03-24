a5148d9b8d0b7bfe51633bd8f77bfb730bdcbc57
hiro <hirokonosub@gmail.com>
2025-03-24 23:42:14 +0900

    トップ画面の実装

--- src/main/java/io/shiftmanager/you/controller/TopContoroller.java
+++ src/main/java/io/shiftmanager/you/controller/TopContoroller.java
@@ -1,2 +1,14 @@
-package io.shiftmanager.you.controller;public class TopContoroller {
+package io.shiftmanager.you.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TopContoroller {
+
+    @GetMapping("/")
+    public String index(){
+        return "top";
+    }
+
 }
