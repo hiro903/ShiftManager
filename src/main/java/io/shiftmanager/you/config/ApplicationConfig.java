5ac7c0bf2b5a02ba0fbc3788cb6f4f09dd377629
hiro <hirokonosub@gmail.com>
2025-04-27 22:51:28 +0900

    設定：ApplicationConfigの追加

---
+++ src/main/java/io/shiftmanager/you/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package io.shiftmanager.you.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+    
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+} 
